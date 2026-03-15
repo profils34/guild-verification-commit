53cf68113cf6abc13ed04234670f78ea210db43b
profils34 <profil31634@hotmail.com>
2026-03-16 00:18:21 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:18:16.498Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-15T21:18:21.787Z
+Commit: Fix broken links in documentation
