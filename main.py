e3850ae41d9d6715d86bd0b1414cfbda31594447
profils34 <profil31634@hotmail.com>
2026-03-16 00:19:04 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:19:01.655Z
-Commit: Add multi-language support
+Timestamp: 2026-03-15T21:19:04.954Z
+Commit: Resolve null pointer exception in user service
