9353bf2230971456ec299beb5b37d6062a9d43c5
profils34 <profil31634@hotmail.com>
2026-03-16 00:19:29 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:19:03.809Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-15T21:19:29.838Z
+Commit: Add multi-language support
