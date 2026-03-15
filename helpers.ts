eb8ef094f5ecf0297efa3c0d95da8089fda4bdef
profils34 <profil31634@hotmail.com>
2026-03-16 00:18:43 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:18:30.138Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-15T21:18:43.931Z
+Commit: Fix memory leak in event handlers
