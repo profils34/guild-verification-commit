b2763c7f3bdd23f299acde9e371b1ee58015b7e1
profils34 <profil31634@hotmail.com>
2026-03-16 00:18:54 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:18:46.426Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-15T21:18:53.984Z
+Commit: Fix memory leak in event handlers
