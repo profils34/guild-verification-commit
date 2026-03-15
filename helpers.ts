404c74b0e42c4dd78272b098eac171e3016f3fed
profils34 <profil31634@hotmail.com>
2026-03-16 00:18:45 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:18:43.931Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-15T21:18:44.992Z
+Commit: Fix broken links in documentation
